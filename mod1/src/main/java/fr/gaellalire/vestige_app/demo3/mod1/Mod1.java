8dd13a972b0c18e106465e468196bbe9cb314206
Gaël Lalire <gael.lalire@gaellalire.fr>
2020-10-11 21:59:06 +0200

    Update unix part of SAL

--- mod1/src/main/java/fr/gaellalire/vestige_app/demo3/mod1/Mod1.java
+++ mod1/src/main/java/fr/gaellalire/vestige_app/demo3/mod1/Mod1.java
@@ -7,8 +7,8 @@ import fr.gaellalire.vestige.spi.job.DummyJobHelper;
 import fr.gaellalire.vestige.spi.resolver.AttachedClassLoader;
 import fr.gaellalire.vestige.spi.resolver.ResolvedClassLoaderConfiguration;
 import fr.gaellalire.vestige.spi.resolver.Scope;
+import fr.gaellalire.vestige.spi.resolver.maven.CreateClassLoaderConfigurationRequest;
 import fr.gaellalire.vestige.spi.resolver.maven.MavenContextBuilder;
-import fr.gaellalire.vestige.spi.resolver.maven.ResolveMavenArtifactRequest;
 import fr.gaellalire.vestige.spi.resolver.maven.ResolveMode;
 import fr.gaellalire.vestige.spi.resolver.maven.VestigeMavenResolver;
 
@@ -27,10 +27,10 @@ public class Mod1 implements Callable<Void> {
         Properties demo3Properties = new Properties();
         demo3Properties.load(Mod1.class.getResourceAsStream("demo3.properties"));
         String version = demo3Properties.getProperty("version");
-        ResolveMavenArtifactRequest request = mavenContextBuilder.build().resolve(ResolveMode.FIXED_DEPENDENCIES, Scope.PLATFORM, "fr.gaellalire.vestige_app.demo3",
-                "demo3.mod2", version, "mod2-plugin");
-        request.setNamedModuleActivated(true);
-        ResolvedClassLoaderConfiguration classLoaderConfiguration = request.execute(DummyJobHelper.INSTANCE);
+        CreateClassLoaderConfigurationRequest createClassLoaderConfiguration = mavenContextBuilder.build().resolve("fr.gaellalire.vestige_app.demo3",
+                "demo3.mod2", version).execute(DummyJobHelper.INSTANCE).createClassLoaderConfiguration("mod2-plugin", ResolveMode.FIXED_DEPENDENCIES, Scope.PLATFORM);
+        createClassLoaderConfiguration.setNamedModuleActivated(true);
+		ResolvedClassLoaderConfiguration classLoaderConfiguration = createClassLoaderConfiguration.execute();
         AttachedClassLoader attach = classLoaderConfiguration.attach();
         try {
             Plugin p = (Plugin) attach.getAttachableClassLoader().getClassLoader().loadClass("fr.gaellalire.vestige_app.demo3.mod2.Mod2").getConstructor().newInstance();
